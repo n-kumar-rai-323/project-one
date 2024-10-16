77430193d627badcdfd0844aaa83b0927b174c6b
Nishan Kumar Rai <infonkumarrai323@gmail.com>
2024-10-16 03:37:03 +0100

    hotel

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,12 @@
 const router =require("express").Router();
+const orderRouter = require("../modules/orders/order.route");
+const roomRouter = require("../modules/rooms/room.route");
+const userRouter = require("../modules/users/user.route");
+
+
+router.use("/api/v1/orders",orderRouter);
+router.use("/api/v1/rooms", roomRouter);
+router.use("/api/v1/users", userRouter);
+
 
 module.exports=router;
