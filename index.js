7132cc3fa04cc5ce08ea5f16215fa94bca5f09a6
Nishan Kumar Rai <infonkumarrai323@gmail.com>
2024-10-16 03:28:58 +0100

    hotel

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 require("dotenv").config()
-
 const express = require("express"); 
+const mongoose= require("mongoose")
+const morgan = require("morgan");
+
 
 const app = express();
-
 const PORT = Number(process.env.PORT) || 7877
+const indexRouter = require("./routes");
+
+mongoose.connect(process.env.DB_URL).then(console.log("Database connected..")).catch((e)=>{console.log("Database Error")})
+app.use(express.json());
+app.use("/resources", express.static("public"));
+app.use(morgan("tiny"));
+
+
+app.use("/", indexRouter)
+
 
 app.listen(PORT,()=>{
     console.log(`Application Running ${PORT}`)
